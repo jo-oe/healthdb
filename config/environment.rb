2121ba134cd1b3dfceb03e7ef1eec4a2bbe6ca25
JO <jo.spam@gmx.net>
2017-02-27 12:07:57 +0100

    none

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-#ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(
-#  :default => '%m.%d.%Y',
-#  :date_time12  => "%m.%d.%Y %I:%M%p",
-#  :date_time24  => "%m.%d.%Y %H:%M",
-#)
